04c1e7c6b007e55b9caeda7724afb42481fbb416
isabellaveronezi <isabellavero96@icloud.com>
2019-10-01 19:41:19 -0300

    create theme

---
+++ app/code/WebJump/Project/Block/Html/Header/Logo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WebJump\Project\Block\Html\Header;
+
+class Logo extends \Magento\Theme\Block\Html\Header\Logo
+{
+    protected function _getLogoUrl()
+    {
+        $folderName = \Magento\Config\Model\Config\Backend\Image\Logo::UPLOAD_DIR;
+        $storeLogoPath = $this->_scopeConfig->getValue(
+            'design/header/logo_src',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        $path = $folderName . '/' . $storeLogoPath;
+        $logoUrl = $this->_urlBuilder
+                ->getBaseUrl(['_type' => \Magento\Framework\UrlInterface::URL_TYPE_MEDIA]) . $path;
+
+        if ($storeLogoPath !== null && $this->_isFile($path)) {
+            $url = $logoUrl;
+        } elseif ($this->getLogoFile()) {
+            $url = $this->getViewFileUrl($this->getLogoFile());
+        } else {
+            $url = $this->getViewFileUrl('images/logo.svg');
+        }
+        return $url;
+    }
+}
